e333b5d34758128075d40b540c378408dad26dc7
Khanhubuntu <pqkhanh561@gmail.com>
2020-05-02 15:52:40 +0700

    server work really well

--- pythonsection/agent.py
+++ pythonsection/agent.py
@@ -10,17 +10,18 @@ import time
 import tensorflow as tf
 from model import model
 
-import socket	
-import time	
-						# Import socket module
+import socket   
+import time     
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)					# Create a socket object
+# Import socket module
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   # Create a socket object
 host = socket.gethostname() # Get local machine name
-port = 12345								# Reserve a port for your service.
-s.bind((host, port))				# Bind to the port
+print(host)
+port = 12345    # Reserve a port for your service.
+s.bind((host, port))    # Bind to the port
 
-s.listen(5)									# Now wait for client connection.
-c, addr = s.accept()			# Establish connection with client.
+s.listen(5) # Now wait for client connection.
+c, addr = s.accept()    # Establish connection with client.
 print ('Got connection from', addr)
 
 env = env(c)
@@ -29,97 +30,98 @@ np.random.seed(0)
 """ Implementation of deep q learning algorithm """ 
 class DQN:
 
-	def __init__(self, action_space, state_space):
+        def __init__(self, action_space, state_space):
 
-		self.action_space = action_space
-		self.state_space = state_space
-		self.epsilon = 1
-		self.gamma = .95
-		self.batch_size = 64
-		self.epsilon_min = .01
-		self.epsilon_decay = .995
-		self.learning_rate = 0.001
-		self.memory = deque(maxlen=100000)
-		self.sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
-		self.model =model(state_space, action_space, self.sess)
+                self.action_space = action_space
+                self.state_space = state_space
+                self.epsilon = 1
+                self.gamma = .95
+                self.batch_size = 64
+                self.epsilon_min = .01
+                self.epsilon_decay = .995
+                self.learning_rate = 0.001
+                self.memory = deque(maxlen=100000)
+                self.sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+                self.model =model(state_space, action_space, self.sess)
 
-	def build_model(self):
-		return model
+        def build_model(self):
+                return model
 
-	def remember(self, state, action, reward, next_state, done):
-		self.memory.append((state, action, reward, next_state, done))
+        def remember(self, state, action, reward, next_state, done):
+                self.memory.append((state, action, reward, next_state, done))
 
-	def act(self, state):
+        def act(self, state):
 
-		if np.random.rand() <= self.epsilon:
-			return random.randrange(self.action_space)
-		act_values = self.model.predict(state)
-		return np.argmax(act_values[0])
+                if np.random.rand() <= self.epsilon:
+                        return random.randrange(self.action_space)
+                act_values = self.model.predict(state)
+                return np.argmax(act_values[0])
 
-	def replay(self):
-		if len(self.memory) < self.batch_size:
-			return
-		minibatch = random.sample(self.memory, self.batch_size)
-		states = np.array([i[0] for i in minibatch])
-		actions = np.array([i[1] for i in minibatch])
-		rewards = np.array([i[2] for i in minibatch])
-		next_states = np.array([i[3] for i in minibatch])
-		dones = np.array([i[4] for i in minibatch])
-		states = np.squeeze(states)
-		next_states = np.squeeze(next_states)
+        def replay(self):
+                if len(self.memory) < self.batch_size:
+                        return
+                minibatch = random.sample(self.memory, self.batch_size)
+                states = np.array([i[0] for i in minibatch])
+                actions = np.array([i[1] for i in minibatch])
+                rewards = np.array([i[2] for i in minibatch])
+                next_states = np.array([i[3] for i in minibatch])
+                dones = np.array([i[4] for i in minibatch])
+                states = np.squeeze(states)
+                next_states = np.squeeze(next_states)
 
-		targets = rewards + self.gamma*(np.amax(self.model.predict_on_batch(next_states), axis=1))*(1-dones)
-		targets_full = self.model.predict_on_batch(states)
-		#print(targets_full)
-		ind = np.array([i for i in range(self.batch_size)])
-		targets_full[[ind], [actions]] = targets
-		#print('target', targets.shape)
-		#print('target_full',targets_full.shape)
-		#print('state',states.shape)
+                targets = rewards + self.gamma*(np.amax(self.model.predict_on_batch(next_states), axis=1))*(1-dones)
+                targets_full = self.model.predict_on_batch(states)
+                #print(targets_full)
+                ind = np.array([i for i in range(self.batch_size)])
+                targets_full[[ind], [actions]] = targets
+                #print('target', targets.shape)
+                #print('target_full',targets_full.shape)
+                #print('state',states.shape)
 
-		ind = np.array([i for i in range(self.batch_size)])
-		targets_full[[ind], [actions]] = targets
-		self.model.fit(states, targets_full, epochs=1, verbose=0)
-		if self.epsilon > self.epsilon_min:
-			self.epsilon *= self.epsilon_decay
+                ind = np.array([i for i in range(self.batch_size)])
+                targets_full[[ind], [actions]] = targets
+                self.model.fit(states, targets_full, epochs=1, verbose=0)
+                if self.epsilon > self.epsilon_min:
+                        self.epsilon *= self.epsilon_decay
 
 def train_dqn(episode):
-	action_dict = {0:'left', 1:'down', 2:'right', 3:'up', 4:'stay'}
-	loss = []
-	agent = DQN(4, 10)
-	for e in range(episode):
-		print("Episode {}".format(e))
-		#state = env.reset() #################
-		state = np.random.rand(1,10)
-		state = np.reshape(state, (1, 10))
-		score = 0
-		max_steps = 1000
-		full_msg=''
-		for i in range(max_steps):
-			action = agent.act(state)
-			for i in range(11):
-				reward, next_state, done, full_msg = env.step(action,full_msg)
-				time.sleep(2)
-			score += reward
+        action_dict = {0:'left', 1:'down', 2:'right', 3:'up', 4:'stay'}
+        loss = []
+        agent = DQN(4, 10)
+        for e in range(episode):
+                print("Episode {}".format(e))
+                #state = env.reset() #################
+                state = np.random.rand(1,10)
+                state = np.reshape(state, (1, 10))
+                score = 0
+                max_steps = 1000
+                full_msg=''
+                for i in range(max_steps):
+                        action = agent.act(state)
+                        for i in range(11):
+                                reward, next_state, done, full_msg = env.step(action,full_msg)
+                                #time.sleep(2)
+                       #print(next_state)
+                        score += reward
 
-			#print([action_dict[action], done, reward])
-			next_state = np.reshape(next_state, (1, 10))
-			agent.remember(state, action, reward, next_state, done)
-			state = next_state
-			agent.replay()
-			if done:
-				print("")
-				print("episode: {}/{}, score: {}".format(e, episode, score))
-				time.sleep(2)
-				break
-		loss.append(score)
-	return loss
+                        #print([action_dict[action], done, reward])
+                        next_state = np.reshape(next_state, (1, 10))
+                        agent.remember(state, action, reward, next_state, done)
+                        state = next_state
+                        agent.replay()
+                        if done:
+                                print("")
+                                print("episode: {}/{}, score: {}".format(e, episode, score))
+                                time.sleep(2)
+                                break
+                loss.append(score)
+        return loss
 
 
 if __name__ == '__main__':
-	ep = 10000
-	loss = train_dqn(ep)
-	plt.plot([i for i in range(ep)], loss)
-	plt.xlabel('episodes')
-	plt.ylabel('reward')
-	plt.show()
+        ep = 10000
+        loss = train_dqn(ep)
+        plt.plot([i for i in range(ep)], loss)
+        plt.xlabel('episodes')
+        plt.ylabel('reward')
+        plt.show()
--- pythonsection/env.py
+++ pythonsection/env.py
@@ -5,104 +5,105 @@ import numpy as np
 HEADERSIZE = 70
 
 class env():
-	filestate = "../state.txt"
+        filestate = "../state.txt"
 
-	def __init__(self, socket):
-		self.state = [] #Include position of agen and position of enemy
-		self.reward = 0
-		self.dead = 0	#1: is dead
-		self.win = 0
-		self.socket = socket
-		for i in range(50):
-			self.socket.send(bytes('right',encoding='utf8'))
-			tmp =''
-			while True:
-				t = self.socket.recv(16)
-				tmp = tmp + t.decode("utf-8")
-				if tmp.count(',')>=3 and tmp[-1]!=',':
-					break
-		try:
-			while self.sock.recv(1024): 
-				pass
-		except:
-			print("empty")
-			pass
-			
-
-
-	def step(self, action, full_msg):
-		self.socket.send(bytes('right',encoding='utf8'))
-		while True:
-			t = self.socket.recv(70)
-			full_msg =full_msg + t.decode("utf-8")
-			if len(full_msg) > HEADERSIZE:
-				break
-		msg = full_msg[:HEADERSIZE]
-		full_msg = full_msg[HEADERSIZE:]
-		print(full_msg)
-		print(msg)
-		print(len(msg))
-		self.state = np.random.rand(1,10)
-		return self.reward, self.state, self.dead, full_msg
-		'''
-		
-		#Change old state to new state
-		oldstate = self.state
-		while (True):
-			try:
-				self.reward = 0
-				self.dead= 0
-				self.state = []
-				self.win = 0
-
-				self.preprocess_statefile(self.filestate)
-				#print(self.state)
-				#print(self.dead)
-				#print("+++++++++++++++++++++++++")
-				break
-			except:
-				continue
-
-		if self.dead==1:
-			self.reward-=3
-		if self.win==1:
-			self.reward+=100
-			self.dead=1
-		self.reward-=0.1
-		#print(self.state[0][0:3])
-		#print(oldstate)
-		if oldstate != []:
-			if self.state[0][0:1] == oldstate[0][0:1] :
-				self.reward-=10
-		subprocess.call('rm ' + self.filestate,shell=True)
-		subprocess.call('touch ' + self.filestate, shell=True)
-		'''
-		return self.reward, self.state, self.dead
+        def __init__(self, socket):
+                self.state = [] #Include position of agen and position of enemy
+                self.reward = 0
+                self.dead = 0   #1: is dead
+                self.win = 0
+                self.socket = socket
+                #for i in range(50):
+                #        self.socket.send(bytes('right',encoding='utf8'))
+                #        tmp =''
+                #        while True:
+                #                t = self.socket.recv(16)
+                #                tmp = tmp + t.decode("utf-8")
+                #                if tmp.count(',')>=3 and tmp[-1]!=',':
+                #                        break
+                #try:
+                #        while len(self.socket.recv(1024)):
+                #            pass
+                #except:
+                #        print("empty")
+                #        pass
 
 
+        def step(self, action, full_msg):
+                self.socket.send(bytes('right',encoding='utf8'))
+                while True:
+                        t = self.socket.recv(128)
+                        full_msg =full_msg + t.decode("utf-8")
+                        if len(full_msg) > HEADERSIZE-1:
+                                break
+                msg = full_msg[:HEADERSIZE]
+                full_msg = full_msg[HEADERSIZE:]
+                #print(full_msg)
+                print(msg)
+                #print(len(msg))
+                print('')
+                print('')
+                self.state = np.random.rand(1,10)
+                return self.reward, self.state, self.dead, full_msg
+                '''
+                
+                #Change old state to new state
+                oldstate = self.state
+                while (True):
+                        try:
+                                self.reward = 0
+                                self.dead= 0
+                                self.state = []
+                                self.win = 0
 
-	def preprocess_statefile(self, filestate):
-		f = open(filestate, "r")
-		pos_a = f.readline()
-		pos_e = f.readline()
-		pos_e = pos_e[:(len(pos_e)-2)]
-		is_dead = f.readline()
-		is_win = f.readline()
-		f.close()
-		pos_a = pos_a + ',' + pos_e
-		self.state.append([float(i) for i in pos_a.split(",")])
-		#self.state.append([float(i) for i in pos_e.split(",")])
-		self.dead = float(is_dead)
-		self.win = float(is_win)
+                                self.preprocess_statefile(self.filestate)
+                                #print(self.state)
+                                #print(self.dead)
+                                #print("+++++++++++++++++++++++++")
+                                break
+                        except:
+                                continue
 
-	def reset(self):
-		_,state,_ = self.step(4) #stay
-		return(state)
+                if self.dead==1:
+                        self.reward-=3
+                if self.win==1:
+                        self.reward+=100
+                        self.dead=1
+                self.reward-=0.1
+                #print(self.state[0][0:3])
+                #print(oldstate)
+                if oldstate != []:
+                        if self.state[0][0:1] == oldstate[0][0:1] :
+                                self.reward-=10
+                subprocess.call('rm ' + self.filestate,shell=True)
+                subprocess.call('touch ' + self.filestate, shell=True)
+                '''
+                return self.reward, self.state, self.dead
+
+
+
+        def preprocess_statefile(self, filestate):
+                f = open(filestate, "r")
+                pos_a = f.readline()
+                pos_e = f.readline()
+                pos_e = pos_e[:(len(pos_e)-2)]
+                is_dead = f.readline()
+                is_win = f.readline()
+                f.close()
+                pos_a = pos_a + ',' + pos_e
+                self.state.append([float(i) for i in pos_a.split(",")])
+                #self.state.append([float(i) for i in pos_e.split(",")])
+                self.dead = float(is_dead)
+                self.win = float(is_win)
+
+        def reset(self):
+                _,state,_ = self.step(4) #stay
+                return(state)
 
 
 #if __name__=="__main__":
-#	 e= env()
-#	 e.reset()
-#	 while(True):
-#		 e.step(1);
-#		 time.sleep(0)
+#        e= env()
+#        e.reset()
+#        while(True):
+#                e.step(1);
+#                time.sleep(0)
